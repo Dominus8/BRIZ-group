3e895a2416c0004257ab2c96f661a99f07ee48de
Dominus8 <ikari162@mail.ru>
2021-09-02 16:03:13 +0300

    preload css & loading="lazy" on image

--- BRIZ/resources/views/base.blade.php
+++ BRIZ/resources/views/base.blade.php
@@ -14,10 +14,15 @@
     <link href="/css/normalize.css" rel="stylesheet" type="text/css" >
     <link href="/css/animate.min.css" rel="stylesheet" type="text/css" >
     <!-- mine css -->
-    <link href="/css/style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="preload" href="/css/style.css" as="style"  type="text/css">
+    <link rel="preload" href="https://unpkg.com/swiper/swiper-bundle.min.css" as="style" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://d3js.org/d3.v7.min.js"></script>
+    <script>
+        window.onload=function(){
+            document.getElementById('mapWrap').style.display='block';
+        }
+    </script>
     
     
     @yield('head-link')
